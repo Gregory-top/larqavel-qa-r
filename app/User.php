c0a7884960034b530b0119cf9759118b09dd5e6a
Gregory-top <hcsclock88@mail.ru>
2020-01-28 01:23:45 +1000

    Prepupulate users and questions tables using model factories and seed

--- app/User.php
+++ app/User.php
@@ -33,11 +33,7 @@ class User extends Authenticatable
         return $this->hasmany(Question::class);
     }
 
-    public fu] setTitleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
-    }
+    
 
     /**
      * The attributes that should be cast to native types.
